b52c15cb1a4efc11b21e8983e104dbdb108582f1
Zeck Li <11781254+zeckli@users.noreply.github.com>
2025-09-01 01:12:03 +0800

    update for SPA mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { vitePlugin as remix } from '@remix-run/dev'
+import { copyFileSync } from 'node:fs'
+import { join } from 'node:path'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+const repo = '/tshue/'
+
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  base: repo,
+  plugins: [
+    remix({
+      basename: repo,
+      ssr: false,
+      buildEnd({ viteConfig }) {
+        if (!viteConfig.isProduction) {
+          return
+        }
+
+        const buildPath = viteConfig.build.outDir
+        copyFileSync(join(buildPath, 'index.html'), join(buildPath, '404.html'))
+      },
+    }),
+    tsconfigPaths(),
+  ],
 })
