4dd4032874260a05af8fc8eb720dc3b3c4060c0d
Zeck Li <11781254+zeckli@users.noreply.github.com>
2025-09-03 02:30:59 +0800

    update styles for head and deco

--- app/components/RootLayout/Head/index.tsx
+++ app/components/RootLayout/Head/index.tsx
@@ -6,9 +6,9 @@ import Logo from '@assets/logo.svg'
 
 const Head = () => {
   const baseCss = clsx('crate text-center')
-  const boxCss = clsx('f-row-cc gap-x-1 pt-14 md:pt-24')
-  const logoCss = clsx('size-14 self-end')
-  const nameCss = clsx('text-5xl font-bold md:text-6xl')
+  const boxCss = clsx('f-row-cc gap-x-1 pt-10 md:pt-24')
+  const logoCss = clsx('size-9 self-end md:size-14')
+  const nameCss = clsx('mr-1 text-4xl font-bold md:text-6xl')
   const descCss = clsx('pb-4 pt-6 text-center text-lg text-grey-30 md:text-xl')
   const breakCss = clsx('block md:hidden')
 
